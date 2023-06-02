a9591ef9298d912bfb08754b09ebb3304a4046cc
Ingrid G <ing.gape18@gmail.com>
2023-06-02 11:55:04 -0500

    refactoring code

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,27 @@
 import { useState } from 'react'
 import { RandomFox } from './component/RandomFox'
 
+//funcion para generar elementos random
 const random = () => Math.floor(Math.random() * 123) + 1;
 
+//2 formas para tipar 
+//1 - array de objetos y cada objeto va a tener id y url
+//type ImageItems = Array<{ id: string, url: string }>;
+//const [images, setImages] = useState <ImageItems>([])
+
+//2 - minima representacion es el item como tal- solo el elemento que esta dentro del [] => useState + <Array<ImageItem>>
+type ImageItem = { id: string, url: string };
+
+//funcion para generar id unicos
+const generateId = () => Math.random().toString(36).substring(2,9)
+    
 const Home = () => {
-    const [images, setImages] = useState<string[]>([
-        `https://randomfox.ca/images/${random()}.jpg`,
-        `https://randomfox.ca/images/${random()}.jpg`,
-        `https://randomfox.ca/images/${random()}.jpg`,
-        `https://randomfox.ca/images/${random()}.jpg`
+    
+    const [images, setImages] = useState <Array<ImageItem>>([
+        { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+        { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+        { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+        { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` }       
     ]);
     
     return (
@@ -19,10 +32,10 @@ const Home = () => {
             </h1>
             <div>
             {
-                    images.map((image, index) => {
+                    images.map((image) => {
                     return(
-                        <div key={index} className='p-4'>
-                            <RandomFox image = {image}/>
+                        <div key={image.id} className='p-4'>
+                            <RandomFox image = {image.url}/>
                         </div>
                     )
                 })
