4c3a6374093e028a28d6b8745c8769d115778552
Ingrid G <ing.gape18@gmail.com>
2023-06-02 10:58:22 -0500

    typing state - simple

--- app/component/RandomFox.tsx
+++ app/component/RandomFox.tsx
@@ -1,11 +1,11 @@
 //2 Explicito - Tipa lo que retorna la funcion (un elemento de React) // es el que se debe usar
 type Props = {image: string}
 
-export const RandomFox = (props:Props): JSX.Element => {
+/* export const RandomFox = (props:Props): JSX.Element => {
      return (
          <img width={320} height='auto' className='rounded' src={props.image} alt='' />
     ) 
-}
+} */
 
 //cambio en tipado
 export const RandomFox = ({image}:Props): JSX.Element => {
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
+'use client';
+import { useState } from 'react'
 import { RandomFox } from './component/RandomFox'
+
 const random = () => Math.floor(Math.random() * 123) + 1;
 
-const Home = ()  => {
+const Home = () => {
+    const [images, setImages] = useState<string[]>([
+        `https://randomfox.ca/images/${random()}.jpg`,
+        `https://randomfox.ca/images/${random()}.jpg`,
+        `https://randomfox.ca/images/${random()}.jpg`,
+        `https://randomfox.ca/images/${random()}.jpg`
+    ]);
+    
     return (
-        <div>
+        <body>
             <h1 className='text-3xl font-bold underline'>
                 Hello world!
             </h1>
-            <RandomFox image= {`https://randomfox.ca/images/${random()}.jpg`}/>
-        </div>
+            <div>
+            {
+                    images.map((image, index) => {
+                    return(
+                        <div key={index} className='p-4'>
+                            <RandomFox image = {image}/>
+                        </div>
+                    )
+                })
+            }
+           </div>
+        </body>
     )
 }
 
