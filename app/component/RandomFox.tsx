295bf61ad6ac5e1d01f260b64c71c82cd46f1ef8
Ingrid G <ing.gape18@gmail.com>
2023-06-02 21:12:24 -0500

    new watcher

--- app/component/RandomFox.tsx
+++ app/component/RandomFox.tsx
@@ -1,4 +1,6 @@
-//2 Explicito - Tipa lo que retorna la funcion (un elemento de React) // es el que se debe usar
+import { useRef, useEffect } from 'react'
+
+//tipado Explicito - Tipa lo que retorna la funcion (un elemento de React) // es el que se debe usar
 type Props = {image: string}
 
 /* export const RandomFox = (props:Props): JSX.Element => {
@@ -8,8 +10,36 @@ type Props = {image: string}
 } */
 
 //cambio en tipado
-export const RandomFox = ({image}:Props): JSX.Element => {
+export const RandomFox = ({ image }: Props): JSX.Element => {
+    //etiqueta de imagen => HTMLImageElement
+    const node = useRef<HTMLImageElement>(null) //ojo:  con TS se inicializa en null siempre que no conozca el valor inicial
+
+    //se ejecuta cuando la imagen es visible en pantalla
+    //(tiene interseccion con el browser)
+    useEffect(() => {
+    //new watcher
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                //on Interseccion => console.log()
+                if (entry.isIntersecting) {
+                    console.log('si funk')
+                }
+            })
+        });
+    
+    //observar nodo
+        if (node.current) {
+           observer.observe(node.current); 
+        }
+        
+    //desconectar
+        return (() => {
+            observer.disconnect()
+        });
+    
+    }, [])
+    
     return (
-        <img width={320} height='auto' className='rounded' src={image} alt='' />
+        <img ref={node} width={320} height='auto' className='rounded' src={image} alt='' />
    ) 
 }
