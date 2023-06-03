823154d27a47ee391c2d69445142ba550038d5ae
Ingrid G <ing.gape18@gmail.com>
2023-06-02 21:33:57 -0500

    gray box working

--- app/component/RandomFox.tsx
+++ app/component/RandomFox.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 
 //tipado Explicito - Tipa lo que retorna la funcion (un elemento de React) // es el que se debe usar
 type Props = {image: string}
@@ -14,6 +14,10 @@ export const RandomFox = ({ image }: Props): JSX.Element => {
     //etiqueta de imagen => HTMLImageElement
     const node = useRef<HTMLImageElement>(null) //ojo:  con TS se inicializa en null siempre que no conozca el valor inicial
 
+    //state - la imagen es fondo gris hasta que se intersecta o es visible en pantalla
+    const transparentImage = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4='
+    const [src, setSrc] = useState(transparentImage)
+    
     //se ejecuta cuando la imagen es visible en pantalla
     //(tiene interseccion con el browser)
     useEffect(() => {
@@ -23,6 +27,7 @@ export const RandomFox = ({ image }: Props): JSX.Element => {
                 //on Interseccion => console.log()
                 if (entry.isIntersecting) {
                     console.log('si funk')
+                    setSrc(image)
                 }
             })
         });
@@ -37,9 +42,16 @@ export const RandomFox = ({ image }: Props): JSX.Element => {
             observer.disconnect()
         });
     
-    }, [])
+    }, [image])
     
     return (
-        <img ref={node} width={320} height='auto' className='rounded' src={image} alt='' />
+        //inicializa con el valor original de src => lazyImage y carga la imagen solo cuadno se intersecta con browser
+        <img
+            ref={node}
+            width={320}
+            height='auto'
+            className='rounded bg-gray-300'
+            src={src}
+        />
    ) 
 }
